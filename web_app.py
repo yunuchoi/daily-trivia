00f805da83a026fd2bb38ed62d6093d7526f7c2e
Yunu Choi <yoonwoochoi00@gmail.com>
2025-07-01 23:39:11 +1200

    feat: Add debugging logs

--- web_app.py
+++ web_app.py
@@ -5,11 +5,13 @@ app = Flask(__name__)
 
 @app.route('/send_daily_trivia')
 def run_trivia():
+    print('run_trivia')
     try:
         subprocess.Popen([
             '/home/yunuchoi/jippilji-daily-trivia/venv-jippilji/bin/python',
             '/home/yunuchoi/jippilji-daily-trivia/jippilji.py'
         ])
+        print("subprocesses added")
         return 'Trivia script started successfully.', 200
     except Exception as e:
         return f'Error: {str(e)}', 500
